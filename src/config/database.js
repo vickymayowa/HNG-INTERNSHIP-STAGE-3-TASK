885549ffc2124dfc8412073b760427555fe16ee3
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-07-08 15:01:41 +0100

    worked on the config

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,23 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const DATABASE_URL = `postgresql://techie_dev_09:IyoWOLFkyFOvxTgF88Z3WBsItSS3oQeZ@dpg-cq3t6obqf0us73dngbi0-a/hng_internship_task_3`;
+const DATABASE_URL =
+  "postgresql://techie_dev_09:IyoWOLFkyFOvxTgF88Z3WBsItSS3oQeZ@dpg-cq3t6obqf0us73dngbi0-a.oregon-postgres.render.com/hng_internship_task_3";
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
-  url: DATABASE_URL,
   logging: false,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // This line may be necessary to avoid self-signed certificate errors
+    },
   },
 });
 
@@ -21,6 +27,6 @@ sequelize
   .then(() =>
     console.log("Database connection has been established successfully.")
   )
-  .catch((err) => console.log("Unable to connect to the database"));
+  .catch((err) => console.log("Unable to connect to the database:", err));
 
 module.exports = sequelize;
